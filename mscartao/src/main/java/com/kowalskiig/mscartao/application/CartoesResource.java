8d59c27fbedb8efe2ec993adb63938ce02e47778
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-10-28 02:02:58 -0300

    feat endpoint Ok, MS de cartao

--- mscartao/src/main/java/com/kowalskiig/mscartao/application/CartoesResource.java
+++ mscartao/src/main/java/com/kowalskiig/mscartao/application/CartoesResource.java
@@ -1,4 +1,15 @@
 package com.kowalskiig.mscartao.application;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cartoes")
 public class CartoesResource {
+    
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
 }
